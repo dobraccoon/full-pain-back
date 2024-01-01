6cc223b7d3b2bf0f1a346b92a43f021f468fe365
Denis Davydov <denis.davydov.personal.email@gmail.com>
2024-01-01 20:19:59 +0300

    project has been initialized

---
+++ src/main/java/com/dobraccoon/fullpainback/FullPainBackApplication.java
@@ -0,0 +1,13 @@
+package com.dobraccoon.fullpainback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullPainBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullPainBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dobraccoon/fullpainback/FullPainBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dobraccoon.fullpainback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullPainBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
